e951a5444c324bfa1629fc371495e332ff4b23b0
Jacky Chow <jackychow@Jackys-MacBook-Pro.local>
2024-10-16 10:50:27 +0800

    add event emitter method

--- src/react/LoginCallBack.js
+++ src/react/LoginCallBack.js
@@ -1,11 +1,11 @@
 /*!
-* Copyright 2024-Present Animoca Brands Corporation Ltd. 
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright 2024-Present Animoca Brands Corporation Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 import React, { useEffect } from 'react';
 
@@ -13,42 +13,34 @@ import { useOCAuth } from './OCContext';
 
 let handledRedirect = false;
 
-const LoginCallBack = ( { successCallback, errorCallback, customErrorComponent, customLoadingComponent } ) =>
-{
-    const { ocAuth, authState, updateAuthError } = useOCAuth();
+const LoginCallBack = ({ successCallback, errorCallback, customErrorComponent, customLoadingComponent }) => {
+    const { ocAuth, authState, setAuthError } = useOCAuth();
 
-    useEffect( () =>
-    {
-        const handleLogin = async () =>
-        {
-            if ( ocAuth )
-            {
-                try
-                {
+    useEffect(() => {
+        const handleLogin = async () => {
+            if (ocAuth) {
+                try {
                     await ocAuth.handleLoginRedirect();
                     successCallback();
-                } catch ( e )
-                {
-                    updateAuthError(e)
-                    if ( errorCallback )
-                    {
+                } catch (e) {
+                    setAuthError(e);
+                    if (errorCallback) {
                         errorCallback();
                     }
                 }
             }
         };
-        if ( !handledRedirect )
-        {
+        if (!handledRedirect) {
             handleLogin();
         }
-    }, [ ocAuth ] );
+    }, [ocAuth]);
 
-    if ( authState.error !== undefined && !errorCallback )
-    {
-        return customErrorComponent ? customErrorComponent : <div>Error Logging in: { authState.error.message }</div>;
-    } else
-    {
-        return customLoadingComponent ? customLoadingComponent : (
+    if (authState.error !== undefined && !errorCallback) {
+        return customErrorComponent ? customErrorComponent : <div>Error Logging in: {authState.error.message}</div>;
+    } else {
+        return customLoadingComponent ? (
+            customLoadingComponent
+        ) : (
             <div>
                 <h3>Loading ......</h3>
             </div>
--- src/sdk/lib/AuthInfoManager.js
+++ src/sdk/lib/AuthInfoManager.js
@@ -1,45 +1,54 @@
 /*!
-* Copyright 2024-Present Animoca Brands Corporation Ltd. 
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright 2024-Present Animoca Brands Corporation Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-class AuthInfoManager
-{
-    _idInfo;
 
-    constructor ()
-    {
+import Emitter from 'tiny-emitter';
+
+const EVENT_AUTH_STATE_CHANGE = 'authStateChange';
+
+class AuthInfoManager {
+    _idInfo;
+    _emmitter;
+
+    constructor() {
         this._authState = null;
         this._idInfo = null;
+        this._emitter = new Emitter();
     }
 
-    setIdInfo ( IdInfo )
-    {
-        this._idInfo = Object.assign( {}, IdInfo );
+    setIdInfo(IdInfo) {
+        this._idInfo = Object.assign({}, IdInfo);
+        this._emitter.emit(EVENT_AUTH_STATE_CHANGE, this._idInfo);
     }
 
-    getIdInfoAttribute ( attribute )
-    {
-        if ( this._idInfo === null )
-        {
+    getIdInfoAttribute(attribute) {
+        if (this._idInfo === null) {
             return undefined;
         }
 
-        return this._idInfo[ attribute ];
+        return this._idInfo[attribute];
     }
 
-    getAuthInfo ()
-    {
-        return Object.assign( {}, this._idInfo );
+    getAuthInfo() {
+        return Object.assign({}, this._idInfo);
     }
 
-    clear ()
-    {
+    clear() {
         this._idInfo = null;
+    }
+
+    subscribe(handler) {
+        this._emitter.on(EVENT_AUTH_STATE_CHANGE, handler);
+    }
+
+    unsubscribe(handler) {
+        this._emitter.off(EVENT_AUTH_STATE_CHANGE, handler);
     }
 }
 
